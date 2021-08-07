b07f907be265228a645754923d1612a1a3b4fd45
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-08-07 12:56:46 +0530

    hello.cpp first program, finished

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//standard namespace assign globally to avoid writing std::
+using namespace std;
+
+int main()
+{
+  std::cout<<"Hello World\n";
+  cout<<"Hello World\n";
+  //example: suppose rahul mom and father give money to rahul so find the total amount of rahul get
+
+  int amount1;
+  cin >> amount1;
+
+  int amount2;
+  cin >> amount2;
+
+  int sum = amount1 + amount2;
+  cout << sum << endl;
+
+  return 0;
+}
